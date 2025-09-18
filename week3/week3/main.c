21f30f15d51c2f97d2a2ec1f4ef349e06938417d
MinhyoungJeong <minhyoung123@naver.com>
2025-09-18 15:58:49 +0900

    01
    실습 1
    • scanf() 사용법 실습
    • int, float 형태의 값을 받아서 그대로 출력해보기

---
+++ week3/week3/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  week3
+//
+//  Created by 정민형 on 9/18/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    
+    int input_int;
+    float input_float;
+    
+    printf("enter an integar: ");
+    scanf("%d",&input_int);
+    
+    printf("enter a float: ");
+    scanf("%f",&input_float);
+    printf("integar : %d, float : %f \n",input_int,input_float);
+    
+    return 0;
+}
